5227188ea03bd7b92321392baad0381e34be6a5a
David Pan <dpan5425@gmail.com>
2020-10-08 17:07:30 -0400

    set up express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/api/config", (req, res) => {
+    res.json({success: true});
+})
+
+app.listen(PORT, () => {
+    console.log("Server listening on http://localhost:" + PORT);
+});
