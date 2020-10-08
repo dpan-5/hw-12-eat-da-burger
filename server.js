2d78b1b235b1c13cd6ec6c05d4c194e19175be45
David Pan <dpan5425@gmail.com>
2020-10-08 17:20:06 -0400

    added express handlebars

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+
 app.get("/api/config", (req, res) => {
     res.json({success: true});
 })
